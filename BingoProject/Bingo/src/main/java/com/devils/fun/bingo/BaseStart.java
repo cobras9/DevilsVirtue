7687bf4675b886fbcd135eb6ec24652474d4365a
Lewis Chao <cobras9@gmail.com>
2014-07-17 10:05:01 +1200

    test

--- BingoProject/Bingo/src/main/java/com/devils/fun/bingo/BaseStart.java
+++ BingoProject/Bingo/src/main/java/com/devils/fun/bingo/BaseStart.java
@@ -9,7 +9,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
-
+Test 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
